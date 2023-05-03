47475ffed0c1360a13ef6796c6adc9c190f77b76
Charles-Rocke <89711262+Charles-Rocke@users.noreply.github.com>
2023-05-03 20:48:47 +0000

    remaking frontend dashboard (apis working)

--- website/auth.py
+++ website/auth.py
@@ -7,6 +7,7 @@ from	typing	import	Dict
 import	json
 import requests
 import base64
+import secrets
 # import	uuid
 #################################
 from	webauthn	import	(
@@ -121,7 +122,7 @@ def	signup():
 # generate registration options
 @auth.route("/generate-registration-options",	methods=["GET"])
 def	handler_generate_registration_options():
-	
+	global current_registration_challenge
 	# print("IN	GENERATE	REG	OPTIONS")
 	# global	current_registration_challenge
 	
@@ -174,23 +175,21 @@ def	handler_generate_registration_options():
 	# return	options_to_json(options)
 	
 
-	global current_registration_challenge
+	# payload
 	payload = {
 		"domain" : rp_id, 
 		"domain_name" : rp_name,
-		"user_uid" : session["user_uid"],
 		"email" : session["email"]
 	}
+	# recieve bloc api response
 	response = requests.get(url="https://bloc-api.bloclabs.repl.co/users/signup", params=payload).json()
-	print(type(response))
+	# json response to get registration challenge 
 	response_object = json.loads(response)
-	print(type(response_object))
+	# set registration challenge
 	current_registration_challenge = response_object["challenge"]
-	print(current_registration_challenge)
-	# decode current_registration_challenge twice for expected challenge
-	encoded = base64.b64encode(current_registration_challenge)
-
-	current_registration_challenge = encoded
+	# decode registration challenge to future expected challenge
+	current_registration_challenge = base64.urlsafe_b64decode(f"{current_registration_challenge}===")
+	# return response to bloc js package
 	return response
 
 
@@ -198,93 +197,122 @@ def	handler_generate_registration_options():
 # verify registration
 @auth.route("/verify-registration-response",	methods=["POST"])
 def	handler_verify_registration_response():
-	print("IN	VERIFY	REG	OPTIONS")
-	# global	current_registration_challenge
-	
+	# print("IN	VERIFY	REG	OPTIONS")
+	# # global	current_registration_challenge
 
+	# get body and send to api to be parsed and verified
+	print(session["email"])
+	print(type(session["email"]))
 	body	=	request.get_data()
 	print("BODY:	",	type(body))
-	print("BODY:	",body)
-	try:
-		credential = RegistrationCredential.parse_raw(body)
-		# check if device used any transports
-		# assign a potentially used transport
-		# user_transports = ''
-		print(current_registration_challenge)
-		verification	=	verify_registration_response(
-				credential=credential,
-				expected_challenge=current_registration_challenge,
-				expected_rp_id=rp_id,
-				expected_origin=origin,
-		)
+	print("payload")
+	payload = {
+		"request" : body,
+		"domain" : rp_id,
+		"origin" : origin,
+		"user" : session["email"],
+	}
+	print("response")
+	# get response from post request and print it
+	response = requests.post(url="https://bloc-api.bloclabs.repl.co/users/verify_signup", params= payload).json()
+	print(response)
+	# # print("BODY:	",body)
+	# # Decode UTF-8 bytes to Unicode, and convert single quotes 
+	# # to double quotes to make it valid JSON
+	# my_json_str = body.decode('utf8').replace("'", '"')
+	# print(my_json_str)
+	# print('- ' * 20)
+	
+	# # Load the JSON to a Python list & dump it back out as formatted JSON
+	# data = json.loads(my_json_str)
+	# print(type(data))
+	# s = json.dumps(data)
+	# print(type(s))
+	
+	# credential = RegistrationCredential.parse_raw(body)
+	# print(type(credential))
+	# 	# check if device used any transports
+	# 	# assign a potentially used transport
+	# 	# user_transports = ''
+	# 	print(current_registration_challenge)
+	# 	verification	=	verify_registration_response(
+	# 			credential=credential,
+	# 			expected_challenge=current_registration_challenge,
+	# 			expected_rp_id=rp_id,
+	# 			expected_origin=origin,
+	# 	)
 
-		print(current_registration_challenge)
+	# 	print(current_registration_challenge)
 		
-		if credential.transports:
-			print("credential:", credential.transports)
-			print("credential type:", type(credential.transports))
-			for transports in credential.transports:
-				print(transports)
-				print(type(transports))
-				print("transports value:", transports.value)
-				print("transports value type:", type(transports.value))
-			# assign the value of the transport to be assigned to the user
-			user_transport = transports.value
-			user_transport_type = transports
-			print("user transport: ", user_transport)
-			print("user transport type: ", user_transport)
+	# 	if credential.transports:
+	# 		print("credential:", credential.transports)
+	# 		print("credential type:", type(credential.transports))
+	# 		for transports in credential.transports:
+	# 			print(transports)
+	# 			print(type(transports))
+	# 			print("transports value:", transports.value)
+	# 			print("transports value type:", type(transports.value))
+	# 		# assign the value of the transport to be assigned to the user
+	# 		user_transport = transports.value
+	# 		user_transport_type = transports
+	# 		print("user transport: ", user_transport)
+	# 		print("user transport type: ", user_transport)
 
 			
-			# create new_user just like new_credential
-			#	log	current	user	in
-			print(f'current user email: {session["email"]}')
-			new_user	=	User(email=session["email"])
-			db.session.add(new_user)
-			db.session.commit()
-			login_user(new_user,	remember=True)
-			flash('Account created!', category = 'success')
-			print("current  user and id: ", current_user.email, current_user.id)
-			# add new credential to current user
-			new_credential	=	WebAuthnCredential(
-					user_id = current_user.id,
-					credential_id=verification.credential_id,
-					credential_public_key=verification.credential_public_key,
-					current_sign_count=verification.sign_count,
-					credential_transport = str(user_transport_type)
+	# 		# create new_user just like new_credential
+	# 		#	log	current	user	in
+	# 		print(f'current user email: {session["email"]}')
+	"""If user is verified """
+	if response["verified"] == True:
+		print(response)
+		new_user	=	User(email=session["email"])
+		db.session.add(new_user)
+		db.session.commit()
+		login_user(new_user,	remember=True)
+		flash('Account created!', category = 'success')
+		return response
+	else:
+		return {"verified":False}
+	# 		print("current  user and id: ", current_user.email, current_user.id)
+	# 		# add new credential to current user
+	# 		new_credential	=	WebAuthnCredential(
+	# 				user_id = current_user.id,
+	# 				credential_id=verification.credential_id,
+	# 				credential_public_key=verification.credential_public_key,
+	# 				current_sign_count=verification.sign_count,
+	# 				credential_transport = str(user_transport_type)
 					
-			)
-			# add credential to database
-			db.session.add(new_credential)
-			db.session.commit()
+	# 		)
+	# 		# add credential to database
+	# 		db.session.add(new_credential)
+	# 		db.session.commit()
 			
-			print("if statement verified")
-			return	{"verified"	:	True}
-		else:
-			#	log	current	user	in
-			print(f'current user email: {session["email"]}')
-			new_user	=	User(email=session["email"])
-			db.session.add(new_user)
-			db.session.commit()
-			login_user(new_user,	remember=True)
-			flash('Account created!', category = 'success')
-			print("current  user and id: ", current_user.email, current_user.id)
-			# add new credential to current user
-			new_credential	=	WebAuthnCredential(
-					user_id = current_user.id,
-					credential_id=verification.credential_id,
-					credential_public_key=verification.credential_public_key,
-					current_sign_count=verification.sign_count
+	# 		print("if statement verified")
+	# 		return	{"verified"	:	True}
+	# 	else:
+	# # 		#	log	current	user	in
+	# 		print(f'current user email: {session["email"]}')
+	# 		new_user	=	User(email=session["email"])
+	# 		db.session.add(new_user)
+	# 		db.session.commit()
+	# 		login_user(new_user,	remember=True)
+	# 		flash('Account created!', category = 'success')
+	# 		print("current  user and id: ", current_user.email, current_user.id)
+	# 		# add new credential to current user
+	# 		new_credential	=	WebAuthnCredential(
+	# 				user_id = current_user.id,
+	# 				credential_id=verification.credential_id,
+	# 				credential_public_key=verification.credential_public_key,
+	# 				current_sign_count=verification.sign_count
 					
-			)
-			# add credential to database
-			db.session.add(new_credential)
-			db.session.commit()
+	# 		)
+	# 		# add credential to database
+	# 		db.session.add(new_credential)
+	# 		db.session.commit()
 			
-			print("else statement verified")
-			return	{"verified"	:	True}
-		
-	except	Exception	as	err:
-			return	{"verified":	False,	"msg":	str(err),	"status":	400}
+	# 		print("else statement verified")
+	# 		return	{"verified"	:	True}
+	
 
 	
 ################
@@ -322,32 +350,47 @@ def	handler_generate_authentication_options():
 	global	current_authentication_challenge
 	
 	
-	# get user from session email
-	print("ASSIGNING	USER")
+	# # get user from session email
+	# print("ASSIGNING	USER")
 	
-	user	=	User.query.filter_by(email=session['email']).first()
-	# who is user?
-	print(user.email)
-	# what is users username?
+	# user	=	User.query.filter_by(email=session['email']).first()
+	# # who is user?
+	# print(user.email)
+	# # what is users username?
 	
 	
-	# get corrent user from session
-	#user = User.query.filter_by(email=session['email']).first()
-	# get the users
-	print(f"USER.CREDENTIALS:	{user.credentials}")
-	options	=	generate_authentication_options(
-			rp_id=rp_id,
-			# allow_credentials=[{
-			# 		"type":	"public-key",
-			# 		"id":	cred.credential_id,
-			# 		"transports":	cred.credential_transport
-			# }	for	cred	in	user.credentials],
-			user_verification=UserVerificationRequirement.REQUIRED,
-	)
+	# # get corrent user from session
+	# #user = User.query.filter_by(email=session['email']).first()
+	# # get the users
+	# print(f"USER.CREDENTIALS:	{user.credentials}")
+	# options	=	generate_authentication_options(
+	# 		rp_id=rp_id,
+	# 		# allow_credentials=[{
+	# 		# 		"type":	"public-key",
+	# 		# 		"id":	cred.credential_id,
+	# 		# 		"transports":	cred.credential_transport
+	# 		# }	for	cred	in	user.credentials],
+	# 		user_verification=UserVerificationRequirement.REQUIRED,
+	# )
 	
-	current_authentication_challenge	=	options.challenge
+	# current_authentication_challenge	=	options.challenge
 	
-	return	options_to_json(options)
+	# return	options_to_json(options)
+	# payload
+	payload = {
+		"domain" : rp_id, 
+		"email" : session["email"]
+	}
+	# recieve bloc api response
+	response = requests.get(url="https://bloc-api.bloclabs.repl.co/users/login", params=payload).json()
+	# json response to get registration challenge 
+	response_object = json.loads(response)
+	# set registration challenge
+	current_authentication_challenge = response_object["challenge"]
+	# decode registration challenge to future expected challenge
+	current_authentication_challenge = base64.urlsafe_b64decode(f"{current_authentication_challenge}===")
+	# return response to bloc js package
+	return response
 
 
 #####################
@@ -359,39 +402,58 @@ def	hander_verify_authentication_response():
 	
 
 	body	=	request.get_data()
+	
+	payload = {
+		"request" : body,
+		"domain" : rp_id,
+		"origin" : origin,
+		"user" : session["email"],
+	}
+	print("response")
+	# get response from post request and print it
+	response = requests.post(url="https://bloc-api.bloclabs.repl.co/users/verify_login", params= payload).json()
 
-	try:
-			credential	=	AuthenticationCredential.parse_raw(body)
+	"""If user is verified """
+	if response["verified"] == True:
+		print(response)
+		user = User.query.filter_by(email=session['email']).first()
+		login_user(user,	remember=True)
+		flash('Account created!', category = 'success')
+		return response
+	else:
+		return {"verified":False}
+	# try:
+	# 		credential	=	AuthenticationCredential.parse_raw(body)
 
-			#	Find	the	user's	corresponding	public	key
+	# 		#	Find	the	user's	corresponding	public	key
 			
-			user = User.query.filter_by(email=session['email']).first()
-			user_credential	=	None
-			for	cred	in	user.credentials:
-					if	cred.credential_id	==	credential.raw_id:
-							user_credential	=	cred
+	# 		user = User.query.filter_by(email=session['email']).first()
+	# 		user_credential	=	None
+	# 		for	cred	in	user.credentials:
+	# 				if	cred.credential_id	==	credential.raw_id:
+	# 						user_credential	=	cred
 
-			if	user_credential	is	None:
-					raise	Exception("Could	not	find	corresponding	public	key	in	DB")
+	# 		if	user_credential	is	None:
+	# 				raise	Exception("Could	not	find	corresponding	public	key	in	DB")
 
-			#	Verify	the	assertion
-			verification	=	verify_authentication_response(
-					credential=credential,
-					expected_challenge=current_authentication_challenge,
-					expected_rp_id=rp_id,
-					expected_origin=origin,
-					credential_public_key=user_credential.credential_public_key,
-					credential_current_sign_count=user_credential.current_sign_count,
-					require_user_verification=True,
-			)
-	except	Exception	as	err:
-			return	{"verified":	False,	"msg":	str(err),	"status":	400}
+	# 		#	Verify	the	assertion
+	# 		verification	=	verify_authentication_response(
+	# 				credential=credential,
+	# 				expected_challenge=current_authentication_challenge,
+	# 				expected_rp_id=rp_id,
+	# 				expected_origin=origin,
+	# 				credential_public_key=user_credential.credential_public_key,
+	# 				credential_current_sign_count=user_credential.current_sign_count,
+	# 				require_user_verification=True,
+	# 		)
+	# except	Exception	as	err:
+	# 		return	{"verified":	False,	"msg":	str(err),	"status":	400}
 
-	#	Update	our	credential's	sign	count	to	what	the	authenticator	says	it	is	now
-	user_credential.current_sign_count = verification.new_sign_count
-	# log user in
-	login_user(user, remember=True)
-	return	{"verified":	True}
+	# #	Update	our	credential's	sign	count	to	what	the	authenticator	says	it	is	now
+	# user_credential.current_sign_count = verification.new_sign_count
+	# # log user in
+	# login_user(user, remember=True)
+	# return	{"verified":	True}
 
 
 ################
