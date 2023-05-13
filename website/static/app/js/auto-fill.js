7da08eb558412296d8868b5a46fa20e517ce94fb
Charles-Rocke <>
2023-05-13 05:40:03 +0000

    Revert "latest commit (up to date analytics)"
    This reverts commit 7d9c9bffbd4aa94d982a40f4a4bac0d4e7055cf8.

--- website/static/app/js/auto-fill.js
+++ website/static/app/js/auto-fill.js
@@ -1,14 +1,14 @@
 // import blocJS auth methods
-// const { browserSupportsWebAuthnAutofill } = blocJS;
+const { browserSupportsWebAuthnAutofill } = blocJS;
 
-// if (!browserSupportsWebAuthnAutofill()) {
-// 	console.log('It seems this browser does not support WebAuthn...');
-// }
+if (!browserSupportsWebAuthnAutofill()) {
+	console.log('It seems this browser does not support WebAuthn...');
+}
 
-// fetch('/generate-authentication-options')
-// .then((options) => {
-// 	// Note the `true` argument here
-// 	startAuthentication(options, true)
-// 		.then(authResp => sendToServerForVerificationAndLogin)
-// 		.catch(err => handleError);
-// });
+fetch('/generate-authentication-options')
+.then((options) => {
+	// Note the `true` argument here
+	startAuthentication(options, true)
+		.then(authResp => sendToServerForVerificationAndLogin)
+		.catch(err => handleError);
+});
