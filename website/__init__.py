cdb8b1ab20616052e46b02e1bb92dc30a2634084
Charles-Rocke <>
2023-05-22 13:24:03 -0400

    Updated __init

--- website/__init__.py
+++ website/__init__.py
@@ -7,18 +7,18 @@ from flask_migrate import Migrate
 
 # Load environment variables based on Docker Compose file
 # if env is from compose testing
-if os.getenv('DOCKER_COMPOSE_FILE') == 'docker-compose-test.yaml':
+if os.getenv('COMPOSE_FILE') == 'docker-compose-test.yaml':
 	load_dotenv('.env.dev')
 	print("loaded test env")
 # if env is from compose production
-elif os.getenv('DOCKER_COMPOSE_FILE') == 'docker-compose-prod.yaml':
+elif os.getenv('COMPOSE_FILE') == 'docker-compose-prod.yaml':
 	load_dotenv('.env.prod')
 	print("loaded prod env")
 else:
 	load_dotenv('.env')
 	print("loaded .env")
 
-print(os.getenv('DOCKER_COMPOSE_FILE'))
+print(os.getenv('COMPOSE_FILE'))
 db = SQLAlchemy()
 DB_NAME = "database.db"
 migrate = Migrate()
