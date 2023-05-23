d539e0bb7aa99e2e900b3efd66c488b36907d2df
Charles-Rocke <>
2023-05-23 13:24:00 -0400

    Import 2

--- website/__init__.py
+++ website/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 import requests
 import json
-from website.render.helper import get_env_variables, update_env_variables
+from .render.helper import get_env_variables, update_env_variables
 
 
 
